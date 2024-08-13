207ffc76aafecba2dbcb793f6c3d454448347594
xiomara <xrodri98@gmail.com>
2024-08-13 02:18:38 -0400

    Revert "first commit"
    This reverts commit 15b5e475a09dfc262e6bb13df85309889b1672f5.

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class AddressForm(FlaskForm):
-    address = StringField(default="enter location", validators=[DataRequired()])
-    submit = SubmitField("Search")
